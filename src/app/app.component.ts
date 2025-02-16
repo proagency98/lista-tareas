6758d6d883baf3400fe98a7d6caf9afcc9322aa0
Luis Flores <luisflores@MacBook-Pro-de-Luis-2.local>
2025-02-16 14:55:29 +0100

    iniciamos el commit y publicacion del proyecto en GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaElementosComponent } from './lista-elementos/lista-elementos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ListaElementosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/lista-elementos/lista-elementos.component.ts
@@ -0,0 +1,25 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-lista-elementos',
+  standalone: true,
+  imports: [NgFor, FormsModule],
+  templateUrl: './lista-elementos.component.html',
+  styleUrl: './lista-elementos.component.css'
+})
+export class ListaElementosComponent {
+  elementos: { id: number, nombre: string}[] = [];
+  nuevoElemento: string = '';
+ 
+  agregarElemento() {
+    if (this.nuevoElemento.trim()) { //verificamos que no esté vacio
+      const nuevoId = this.elementos.length + 1;
+
+      this.elementos.push({id: nuevoId, nombre: this.nuevoElemento}); //agregamos el elemento
+      
+      this.nuevoElemento = ''; // limpiamos el input despues de agregar el elemento
+    }
+  }
+}
